76429e091db60b1d37821f11171527b959a635b5
AfanJM <afanador1106@gmail.com>
2024-03-11 14:56:25 -0500

    envs modificadas

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -3,8 +3,8 @@ import {get} from 'env-var'
 
 export const envs = {
 
-    PORT: get('port').required().asPortNumber(),
-    PUBLIC_PATH: get('publicPath').default('public').asString()
+    PORT: get('PORT').required().asPortNumber(),
+    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
 
 
 }
